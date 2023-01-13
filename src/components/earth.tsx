5bdc4c63419659e0447332292c3565ba7c9b7398
Eoin <eoin@comcast.net>
2023-01-13 01:49:07 -0500

    Chromium updates broke this i think lol

--- src/components/earth.tsx
+++ src/components/earth.tsx
@@ -32,11 +32,11 @@ float map(float value, float min1, float max1, float min2, float max2) {
   return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
 }
 
-float normalize(float v) { return map(v, -1.0, 1.0, 0.0, 1.0); }
+float e_normalize(float v) { return map(v, -1.0, 1.0, 0.0, 1.0); }
 
 void main() {
   vUv2 = uv;
-  vDist = clamp(pow(normalize(dot(normalize(uLight) * vec3(-1.,1.,-1.) , position) * 2.), 1.), 0., 1.);
+  vDist = clamp(pow(e_normalize(dot(e_normalize(uLight) * vec3(-1.,1.,-1.) , position) * 2.), 1.), 0., 1.);
 }
 `;
 
